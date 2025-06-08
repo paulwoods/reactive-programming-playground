7c1883ab59592672ece9090194acfae255f06265
mrpaulwoods <mr.paul.woods@gmail.com>
2025-06-08 13:23:01 -0500

    feat(flux): add example for custom Flux generation with Flux.create
    - Introduced Lec02FluxCreateRefactor to demonstrate Flux.create usage.
    - Added NameGenerator to generate dynamic data for Flux emission.

---
+++ src/main/java/org/mrpaulwoods/sec04/Lec02FluxCreateRefactor.java
@@ -0,0 +1,20 @@
+package org.mrpaulwoods.sec04;
+
+import org.mrpaulwoods.common.Util;
+import org.mrpaulwoods.sec04.helper.NameGenerator;
+import reactor.core.publisher.Flux;
+
+public class Lec02FluxCreateRefactor {
+
+    public static void main(String[] args) {
+
+        var generator = new NameGenerator();
+        var flux = Flux.create(generator);
+        flux.subscribe(Util.subscriber());
+
+        for (int i = 0; i < 10; i++) {
+            generator.generate();
+        }
+    }
+
+}
