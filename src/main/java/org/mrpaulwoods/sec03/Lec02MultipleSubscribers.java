891899a8fb4da72d0d6c87f7e25331be6c7c7e40
mrpaulwoods <mr.paul.woods@gmail.com>
2025-06-07 19:22:58 -0500

    feat(flux): add example for multiple subscribers with `Flux`
    - Introduced `Lec02MultipleSubscribers` to demonstrate multiple `Flux` subscribers.
    - Included examples with filtering and mapping operations for subscriber customization.

---
+++ src/main/java/org/mrpaulwoods/sec03/Lec02MultipleSubscribers.java
@@ -0,0 +1,23 @@
+package org.mrpaulwoods.sec03;
+
+import org.mrpaulwoods.common.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec02MultipleSubscribers {
+    public static void main(String[] args) {
+
+        var flux = Flux.just(1, 2, 3, 4, 5, 6);
+
+        flux.subscribe(Util.subscriber("sub1"));
+
+        flux
+                .filter(i -> i > 7)
+                .subscribe(Util.subscriber("sub2"));
+
+        flux
+                .filter(i -> i % 2 == 0)
+                .map(i -> i + "a")
+                .subscribe(Util.subscriber("sub3"));
+    }
+
+}
