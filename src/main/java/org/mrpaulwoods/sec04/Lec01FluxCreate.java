e38d69c4cfc6b07e6b17c7f14957c40d11e1b6cf
mrpaulwoods <mr.paul.woods@gmail.com>
2025-06-08 13:15:57 -0500

    feat(flux): add example for creating Flux with Flux.create
    - Introduced Lec01FluxCreate to demonstrate dynamic Flux generation.
    - Included example generating country names until Canada is encountered.

---
+++ src/main/java/org/mrpaulwoods/sec04/Lec01FluxCreate.java
@@ -0,0 +1,22 @@
+package org.mrpaulwoods.sec04;
+
+import org.mrpaulwoods.common.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec01FluxCreate {
+
+    public static void main(String[] args) {
+        Flux.create(fluxSink -> {
+                    String country;
+                    do {
+                        country = Util.faker().country().name();
+                        fluxSink.next(country);
+
+
+                    } while (!country.equalsIgnoreCase("Canada"));
+                })
+                .subscribe(Util.subscriber());
+
+
+    }
+}
