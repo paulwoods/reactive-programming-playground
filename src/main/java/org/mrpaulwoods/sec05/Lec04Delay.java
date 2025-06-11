5aa3796a44035e1a4a736f77c01ae3834903ba91
mrpaulwoods <mr.paul.woods@gmail.com>
2025-06-10 21:27:00 -0500

    feat(flux): add example demonstrating delayElements usage
    - Introduced Lec04Delay to showcase the use of delayElements operator with Flux.
    - Added example delaying emission of items by 1 second and subscribing with a custom subscriber.

---
+++ src/main/java/org/mrpaulwoods/sec05/Lec04Delay.java
@@ -0,0 +1,17 @@
+package org.mrpaulwoods.sec05;
+
+import org.mrpaulwoods.common.Util;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class Lec04Delay {
+    public static void main(String[] args) {
+        Flux.range(1, 10)
+                .log()
+                .delayElements(Duration.ofSeconds(1))
+                .subscribe(Util.subscriber());
+
+        Util.sleepSeconds(12);
+    }
+}
