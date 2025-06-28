17de4f5d832fe7c4afb5506ccb9e175b81578448
mrpaulwoods <mr.paul.woods@gmail.com>
2025-06-28 18:52:32 -0500

    feat(flux): add flatMap example with ExternalServiceClient for product retrieval
    - Introduced Lec12FluxFlatMapAssignment to demonstrate usage of Flux.flatMap for processing product data non-blockingly.
    - Utilized ExternalServiceClient to fetch product information and Util.subscriber for subscription handling.
    - Included a demonstration with Flux.range and sleepSeconds for consistent execution.

---
+++ src/main/java/org/mrpaulwoods/sec09/Lec12FluxFlatMapAssignment.java
@@ -0,0 +1,21 @@
+package org.mrpaulwoods.sec09;
+
+import org.mrpaulwoods.common.Util;
+import org.mrpaulwoods.sec09.assignment.ExternalServiceClient;
+import reactor.core.publisher.Flux;
+
+public class Lec12FluxFlatMapAssignment {
+
+    public static void main(String[] args) {
+
+        var client = new ExternalServiceClient();
+
+        Flux.range(1, 10)
+                .flatMap(client::getProduct)
+                .subscribe(Util.subscriber());
+
+        Util.sleepSeconds(20);
+    }
+
+
+}
